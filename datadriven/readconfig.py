a34481abc6cc6b2c9f3edda252364df3de4882a8
haritha <haritharavichandran11@gmail.com>
2026-06-09 20:28:16 +0530

    readerfile

--- datadriven/readconfig.py
+++ datadriven/readconfig.py
@@ -1,7 +1,12 @@
+import os
 from configparser import ConfigParser
 
-def get_config(category,key):
-    config=ConfigParser()
-    config.read("./config.ini")
-    return config.get(category,key)
+def get_config(category, key):
+    config = ConfigParser()
 
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, "..", "config.ini")
+
+    config.read(file_path, encoding="utf-8")
+
+    return config.get(category, key)
