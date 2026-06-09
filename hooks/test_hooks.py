dc11d3de0fc145cc0a59666fc2356b6373d7a037
haritha <haritharavichandran11@gmail.com>
2026-06-09 11:48:07 +0530

    reqiurement added

--- hooks/test_hooks.py
+++ hooks/test_hooks.py
@@ -17,10 +17,7 @@ def test_valid_product():
     driver.find_element(By.NAME, "search").send_keys("HP")
     driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
 
-    check.is_true(
-        driver.find_element(By.LINK_TEXT, "HP LP3065").is_displayed(),
-        "Product not displayed"
-    )
+    check.is_true(driver.find_element(By.LINK_TEXT, "HP LP3065").is_displayed(),"Product not displayed")
 
     check.equal(driver.title, "Search", "Title mismatch")
 
@@ -29,18 +26,11 @@ def test_invalid_product():
     driver.find_element(By.NAME, "search").send_keys("XYZ123")
     driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
 
-    message = driver.find_element(
-        By.XPATH,
-        "//p[contains(text(),'There is no product')]"
-    ).text
-
+    message = driver.find_element(By.XPATH,"//p[contains(text(),'There is no product')]").text
     check.is_in("no product", message.lower())
     check.equal(driver.title, "Search", "Title mismatch")
-
-
 def test_noproduct():
     driver.find_element(By.NAME, "search").send_keys("")
     driver.find_element(By.XPATH, "//button[contains(@class,'btn-default')]").click()
-
     check.is_in("Search", driver.title)
     check.is_true(driver.current_url.endswith("search"))
