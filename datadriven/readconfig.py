10ca55de9a402c56100215a40ee6216fe485afdb
haritha <haritharavichandran11@gmail.com>
2026-06-09 20:31:53 +0530

    reader file changs

--- datadriven/readconfig.py
+++ datadriven/readconfig.py
@@ -1,5 +1,5 @@
-import os
 from configparser import ConfigParser
+import os
 
 def get_config(category, key):
     config = ConfigParser()
@@ -7,6 +7,11 @@ def get_config(category, key):
     base_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(base_dir, "..", "config.ini")
 
-    config.read(file_path, encoding="utf-8")
+    print("LOADING CONFIG FROM:", file_path)
+
+    read_files = config.read(file_path, encoding="utf-8")
+
+    print("CONFIG READ RESULT:", read_files)
+    print("AVAILABLE SECTIONS:", config.sections())
 
     return config.get(category, key)
