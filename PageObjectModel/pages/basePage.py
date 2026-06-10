6315c954e6c6bdebe348eb603882bbb59570513a
haritha <haritharavichandran11@gmail.com>
2026-06-10 14:44:33 +0530

    pom files

--- PageObjectModel/pages/basePage.py
+++ PageObjectModel/pages/basePage.py
@@ -7,23 +7,15 @@ class BasePage:
         self.driver = driver
 
     def click(self, locator):
-        WebDriverWait(self.driver,10).until(
-            EC.element_to_be_clickable(locator)
-        ).click()
+        WebDriverWait(self.driver,10).until(EC.element_to_be_clickable(locator)).click()
 
     def type(self, locator, text):
-        element = WebDriverWait(self.driver,10).until(
-            EC.visibility_of_element_located(locator)
-        )
+        element = WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator))
         element.clear()
         element.send_keys(text)
 
     def is_displayed(self, locator):
-        return WebDriverWait(self.driver,10).until(
-            EC.visibility_of_element_located(locator)
-        ).is_displayed()
+        return WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).is_displayed()
 
     def get_text(self, locator):
-        return WebDriverWait(self.driver,10).until(
-            EC.visibility_of_element_located(locator)
-        ).text
+        return WebDriverWait(self.driver,10).until(EC.visibility_of_element_located(locator)).text
